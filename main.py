d1851b16038ae73aa8c2afc39bd94bd3df71ce96
Emily Addy <eaddy0820@gmail.com>
2025-07-25 14:26:27 -0400

    feat: added read_sheep get

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from sheep_compendium.models.db import db
+from sheep_compendium.models.models import Sheep
 
 app = FastAPI()
 
+@app.get("/sheep/{id}", response_model=Sheep)
+def read_sheep(id: int):
+    return db.get_sheep(id)
